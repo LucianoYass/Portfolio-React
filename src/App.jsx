5a18ce9e19e958971597735cad5ff2c2529fb018
Luciano Cordoba <YassLuciano>
2023-02-26 13:59:53 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+import Header from './componentes/header/Header';
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App;
