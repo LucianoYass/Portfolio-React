8e113ec85f802e4a45a10c7432bdcdcdb76941dd
Luciano Cordoba <YassLuciano>
2023-02-27 15:03:07 -0300

    componente inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import "./App.css";
 import Header from './componentes/header/Header';
+import Home from './componentes/home/Home';
+import About from './componentes/about/About';
 
 const App = () => {
   return (
     <>
     <Header/>
+
+    <main className='main'>
+      <Home/>
+      <About/>
+    </main>
     </>
   )
 }
---
+++ src/componentes/home/Data.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Data = () => {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">Luciano Córdoba</h1>
+        <h3 className="home__subtitle">Desarrollador Frontend</h3>
+        <p className="home__description">Estudiando Desarrollo Frontend en Coderhouse y Tecnicatura en Programacion en Universidad de Quilmes. Siempre me mantengo aprendiendo.</p>
+
+        <a href="#contact" className="button button--flex">
+            Contactame!
+        </a>
+    </div>
+  );
+};
+
+export default Data;
